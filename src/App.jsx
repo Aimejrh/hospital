3101d445a080d6800a764eed0a59ae7c108817c1
Aimejrh <ameerimzanbarakova@gmail.com>
2024-05-10 19:48:16 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import AppRouter from "./pages/AppRouter/AppRouter";
+
+function App() {
+
+  return (
+    <> 
+      <AppRouter />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/nav/nav.jsx
@@ -0,0 +1,4 @@
+const nav = () => {
+  return <nav></nav>
+}
+export default nav;
